c102017fe15b18a543973c33c350f4926ae21ef3
Athul Joy <a.athuljoy@gmail.com>
2022-01-21 14:44:18 +0530

    Update config details

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const express   = require('express')
-const dotenv    = require('dotenv')
-const connectDB = require('./config/db')
-
-// Loding Config
-dotenv.config({ path:'./config/config.env' })
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
 
 // Datebase Connection
-connectDB()
+connectDB();
 
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json({extented: false}))
-
-
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extented: false }));
 
 // Routes
 //app.use('/', require('./routes/index'));
 app.use('/', require('./routes/url'));
 
-const PORT = process.env.PORT
-app.listen(PORT,()=> console.log(`Server Started at PORT: ${PORT}`))
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
